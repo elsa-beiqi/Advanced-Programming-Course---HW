2d6623bc3744a5a7ad4f39a7c9aa0ee70511565a
elsa-beiqi <53520062+elsa-beiqi@users.noreply.github.com>
2020-11-21 00:11:56 +0100

    Create TrieIterator.cpp

---
+++ Iterator and Operator Overloading/TrieIterator.cpp
@@ -0,0 +1,27 @@
+#include "Stack.h"
+#include "Trie.h"
+#include "TrieIterator.h"
+
+
+TrieIterator::TrieIterator(const Trie & myTrie)
+	: myTrie(myTrie), currentNode(nullptr), currentWord("") {
+}
+
+// TODO: Implement this function
+void TrieIterator::Init() {
+
+}
+// TODO: Implement this function
+
+void TrieIterator::Next() {
+
+}
+// TODO: Implement this function
+
+bool TrieIterator::HasMore() const {
+
+}
+// TODO: Implement this function
+string TrieIterator::Current() const {
+
+}
